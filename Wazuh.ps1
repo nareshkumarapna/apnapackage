78da77e3bec0a09042108e8c7a4d7ed5c4e3bb36
nareshkumarapna <109291559+nareshkumarapna@users.noreply.github.com>
2022-07-15 18:33:59 +0530

    Update Wazuh.ps1

--- Wazuh.ps1
+++ Wazuh.ps1
@@ -1,3 +1,4 @@
 ﻿Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.3.5-1.msi `
 -OutFile ${env:tmp}\wazuh-agent-4.3.5.msi; msiexec.exe /i ${env:tmp}\wazuh-agent-4.3.5.msi /q WAZUH_MANAGER='34.93.97.189' WAZUH_REGISTRATION_SERVER='34.93.97.189' WAZUH_AGENT_GROUP='default'
+Start-Sleep -Seconds 15
 Start-Service -Name WazuhSvc
